2cfde628cd6ab187a8ba26c0f700f142e3e08229
Andrew Gabriel <andrewsantf@gmail.com>
2025-08-28 13:43:50 -0300

    fix: aplica migrations no startup

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from app import create_app
+from flask_migrate import upgrade
 
 app = create_app()
 
+# aplica migrations automaticamente quando o Render iniciar
+with app.app_context():
+    upgrade()
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
